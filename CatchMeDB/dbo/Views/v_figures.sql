db3b9c240008aa7183f14fab720cc931ec1e268d
giopl <giopl4@gmail.com>
2016-07-01 21:38:52 +0400

    adding ViewFigures

---
+++ CatchMeDB/dbo/Views/v_figures.sql
@@ -0,0 +1,19 @@
+﻿--use CatchMeDB
+
+CREATE view v_figures as 
+
+select 
+
+project_id,
+count(1) total,
+
+sum(case when status_code < 9 and getDAte() <= due_Date then 1 else 0 end) [open],
+sum(case when status_code < 9 and getDAte() > due_Date then 1 else 0 end) [overdue],
+
+
+sum(case when updated_on <= due_Date and status_code  = 9 then 1 else 0 end) closed_ontime,
+
+sum(case when updated_on > due_Date and status_code  = 9 then 1 else 0 end) closed_late
+
+from v_tasks
+group by project_id
