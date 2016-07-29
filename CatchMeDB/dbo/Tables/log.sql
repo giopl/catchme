887b7853eece8ce1ae39c24a18e1f9ec561f8fc4
giopl <giopl4@gmail.com>
2016-07-29 14:59:23 +0400

    catchMe

--- CatchMeDB/dbo/Tables/log.sql
+++ CatchMeDB/dbo/Tables/log.sql
@@ -1,15 +1,17 @@
 ﻿CREATE TABLE [dbo].[log] (
-    [log_id]      INT             IDENTITY (100, 1) NOT NULL,
-    [user_id]     INT             NOT NULL,
-    [task_id]     INT             NOT NULL,
-    [operation]   NVARCHAR (60)   NULL,
-    [type]        NVARCHAR (60)   NULL,
-    [logtime]     DATETIME        DEFAULT (getdate()) NULL,
-    [description] NVARCHAR (1000) NULL,
+    [log_id]      INT            IDENTITY (100, 1) NOT NULL,
+    [user_id]     INT            NOT NULL,
+    [task_id]     INT            NOT NULL,
+    [operation]   NVARCHAR (60)  NULL,
+    [type]        NVARCHAR (60)  NULL,
+    [logtime]     DATETIME       DEFAULT (getdate()) NULL,
+    [description] NVARCHAR (MAX) NULL,
     PRIMARY KEY CLUSTERED ([log_id] ASC),
     CONSTRAINT [FK_log_task_id] FOREIGN KEY ([task_id]) REFERENCES [dbo].[task] ([task_id]),
     CONSTRAINT [FK_log_user_id] FOREIGN KEY ([user_id]) REFERENCES [dbo].[user] ([user_id])
 );
+
+
 
 
 
