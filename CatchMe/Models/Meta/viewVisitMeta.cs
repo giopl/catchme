8691c2e547f2f8b9f0eb85b05f6afe91a6f448c3
giopl <giopl4@gmail.com>
2016-08-05 17:15:01 +0400

    visits added time ago

--- CatchMe/Models/Meta/viewVisitMeta.cs
+++ CatchMe/Models/Meta/viewVisitMeta.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
+using System.Text;
 using System.Web;
 
 namespace CatchMe.Models
@@ -38,6 +39,53 @@ namespace CatchMe.Models
 
             }
         }
+
+
+        public string LastUpdatedSince
+        {
+
+            get
+            {
+                StringBuilder result = new StringBuilder();
+                if (lastlogin.HasValue)
+                {
+
+
+
+                    TimeSpan t = DateTime.Now - lastlogin.Value;
+
+                    var seconds = t.TotalSeconds;
+                    var minutes = t.TotalMinutes;
+                    var hours = t.TotalHours;
+                    var days = t.TotalDays;
+
+
+                    if (seconds < 60.0)
+                    {
+                        result.AppendFormat("<span class='label label-danger'><b>{0}</b>s ago</label>", (int)seconds);
+                    }
+
+                    if (seconds >= 60.0 && minutes < 60)
+                    {
+                        result.AppendFormat("<span class='label label-danger'><b>{0}</b>m ago</label>", (int)minutes);
+                    }
+
+                    if (minutes >= 60.0 && hours < 24)
+                    {
+                        result.AppendFormat("<span class='label label-warning'><b>{0}</b>h ago</label>", (int)hours);
+                    }
+                    if (hours >= 24.0 && days <= 5)
+                    {
+                        result.AppendFormat("<span class='label label-primary'><b>{0}</b> day(s) ago</label>", (int)days);
+                    }
+                }
+                return result.ToString();
+
+            }
+
+
+        }
+
 
 
     }
